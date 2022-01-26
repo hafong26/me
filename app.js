0d3cf5d1a97a756ff6c9e2dd8245808a4d8f9dc2
Nguyễn Văn Hào <85483297+hafong26@users.noreply.github.com>
2022-01-26 18:35:22 +0700

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const $ = document.querySelector.bind(document)
-var btnMenu = $('.btn-menu');
-var nav = $('.nav')
-btnMenu.onclick = function() {
-    this.classList.toggle('open')
-    nav.classList.toggle('active')
-}
